ef3c1a9001d05639c1e81045c513d5b396b18f60
Mirmik <Mirage666@yandex.ru>
2016-06-10 22:41:04 +0300

    a

--- include/genos/datastruct/hashtable.h
+++ include/genos/datastruct/hashtable.h
@@ -1,22 +1,22 @@
-#ifndef UTIL_HASHTABLE_H_
-#define UTIL_HASHTABLE_H_
+#ifndef UTIL_HASHTABLE_HEAD_H_
+#define UTIL_HASHTABLE_HEAD_H_
 
 #include "util/array.h"
+#include "genos/datastruct/slink_head.h"
 
 //Тип хэш-функции.
-typedef size_t (*ht_hash_ft)(void *key);
+typedef size_t (*ht_hash_ft)(const void *key);
 
 //Тип функции сравнения элементов (возвращает ноль, если равно).
-typedef int (*ht_cmp_ft)(void *key1, void *key2);
+typedef int (*ht_cmp_ft)(const void *key1, const void *key2);
 
-struct hashtable_item
+struct hashtable_link
 {
 	slink_head link;
-	void* key;
-	void* value;
+	const void* key;
 };
 
-struct hashtable
+struct hashtable_head
 {
 	slink_head* table;
 	size_t table_size;
@@ -25,7 +25,7 @@ struct hashtable
 	size_t total;
 };
 
-struct hashtable* hashtable_init(struct hashtable* ht, void* buf, unsigned int len,
+struct hashtable_head* hashtable_head_init(struct hashtable_head* ht, void* buf, unsigned int len,
 	ht_hash_ft hsh, ht_cmp_ft cmp)
 {
 	ht->table = (struct slink_head *) buf;
@@ -35,33 +35,31 @@ struct hashtable* hashtable_init(struct hashtable* ht, void* buf, unsigned int l
 	ht->table_size = len / sizeof(struct slink_head); 
 
 	struct slink_head* l;
-	array_for_each_ptr(l,ht->table,ht->table_size) 
+	array_for_each_ptr(l, ht->table, ht->table_size) 
 		slink_init(l);
 
 	return ht;
 };
 
-struct hashtable_item * hashtable_item_init(
-		struct hashtable_item *item, void *key, void *value)
+struct hashtable_link * hashtable_link_init(
+		struct hashtable_link *item, const void *key)
 {
 	item->key = key;
-	item->value = value; 
 	slink_init(&item->link);
 	return item;
 };
 
-int hashtable_put_to_cell(struct hashtable* ht, struct hashtable_item* item, size_t index)
+int hashtable_put_to_cell(struct hashtable_head* ht, struct hashtable_link* item, size_t index)
 {
 	slink_connect_after(&item->link, ht->table + index);
 }
 
-int hashtable_put(struct hashtable* ht, struct hashtable_item* item)
+int hashtable_put(struct hashtable_head* ht, struct hashtable_link* item)
 {
 	assert(ht);
 	assert(item);
 	assert(ht->table_size);
 	assert(item->key);
-	assert(item->value);
 
 	size_t index = ht->hsh(item->key) % ht->table_size;
 
@@ -70,15 +68,16 @@ int hashtable_put(struct hashtable* ht, struct hashtable_item* item)
 	hashtable_put_to_cell(ht, item, index);
 }
 
-struct hashtable_item *hashtable_get(struct hashtable *ht, void* key)
+struct hashtable_link *hashtable_get(struct hashtable_head *ht, const void* key)
 {
 	assert(ht);
 	assert(ht->table_size);
 	assert(key);
 
 	size_t index = ht->hsh(key) % ht->table_size;
-	struct hashtable_item * it;
+	struct hashtable_link * it;
 
+	if ((ht->table + index)->next == 0) return 0;
 	slink_for_each_entry(it, ht->table + index, link)
 	{
 		if (ht->cmp(key,it->key) == 0) return it;  
@@ -86,14 +85,14 @@ struct hashtable_item *hashtable_get(struct hashtable *ht, void* key)
 	return 0;
 }
 
-struct hashtable_item *hashtable_del(struct hashtable *ht, void* key)
+struct hashtable_link *hashtable_del(struct hashtable_head *ht, void* key)
 {
 	assert(ht);
 	assert(ht->table_size);
 	assert(key);
 
 	size_t index = ht->hsh(key) % ht->table_size;
-	struct hashtable_item * it;
+	struct hashtable_link * it;
 	struct slink_head * sl;
 
 	slink_for_each_entry_safe(it, sl, ht->table + index, link)
